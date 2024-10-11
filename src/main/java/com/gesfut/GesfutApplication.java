6fa00a84e0fc66166a586b288357133165d51f85
Rafael Toresani <rtoresani@gmail.com>
2024-10-11 16:53:55 -0300

    add: variables de entorno

---
+++ src/main/java/com/gesfut/GesfutApplication.java
@@ -0,0 +1,13 @@
+package com.gesfut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GesfutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GesfutApplication.class, args);
+	}
+
+}
