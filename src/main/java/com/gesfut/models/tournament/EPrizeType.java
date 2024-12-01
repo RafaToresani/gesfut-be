b5e3f10fa627962ae0ea3102671cedfa8c34c5e7
Rafael Toresani <rtoresani@gmail.com>
2024-12-01 17:57:56 -0300

    change: enum prize type position

--- src/main/java/com/gesfut/models/tournament/EPrizeType.java
+++ src/main/java/com/gesfut/models/tournament/EPrizeType.java
@@ -1,8 +1,8 @@
 package com.gesfut.models.tournament;
 
 public enum EPrizeType {
-    CHAMPION,         // Para el campeón
-    TOP_SCORER,       // Para el jugador goleador
-    FAIR_PLAY,        // Para el equipo fair play
-    BEST_GOALKEEPER   // Para el mejor arquero
+    POSITION,
+    TOP_SCORER,
+    FAIR_PLAY,
+    BEST_GOALKEEPER
 }
